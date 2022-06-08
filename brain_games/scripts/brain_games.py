7260b09d0ebf4f1d63f29bf70d5378dedbfc30ee
ierofeev <ivanerofeev@bk.ru>
2022-06-08 15:34:05 +0300

    Add user greeting

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python
+from brain_games.cli import welcome_user
+
 
 def greet():
     print('Welcome to the Brain Games!')
@@ -6,6 +8,7 @@ def greet():
 
 def main():
     greet()
+    welcome_user()
 
 
 if __name__ == '__main__':
